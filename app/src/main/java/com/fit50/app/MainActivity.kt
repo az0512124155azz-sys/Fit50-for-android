cac683d1f8ff68f11835e6b9252481226ffc2811
Avishai Tzabary <az0512124155azz@gmail.com>
2026-09-19 23:27:12 +0300

    Add app/src/main/java/com/fit50/app/MainActivity.kt

---
+++ app/src/main/java/com/fit50/app/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.fit50.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent { Fit50App() }
+    }
+}
