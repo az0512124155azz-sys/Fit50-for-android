e7df3475132579e819b8eae53c114b093b95a376
Avishai Tzabary <az0512124155azz@gmail.com>
2026-09-20 08:54:55 +0300

    Run the approved Fit50 UI inside Android

--- app/src/main/java/com/fit50/app/MainActivity.kt
+++ app/src/main/java/com/fit50/app/MainActivity.kt
@@ -1,44 +1,50 @@
 package com.fit50.app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
-        setContent {
-            Fit50Theme {
-                var showSplash by remember { mutableStateOf(true) }
-
-                LaunchedEffect(Unit) {
-                    delay(900)
-                    showSplash = false
-                }
-
-                if (showSplash) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(Forest),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Fit50Logo()
-                    }
-                } else {
-                    Fit50App()
-                }
-            }
+        webView = WebView(this).apply {
+            setBackgroundColor(android.graphics.Color.rgb(250, 247, 242))
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.databaseEnabled = true
+            settings.allowFileAccess = true
+            settings.allowContentAccess = true
+            settings.mediaPlaybackRequiresUserGesture = false
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
+            addJavascriptInterface(Fit50WebBridge(this@MainActivity, this), "Fit50Native")
+            loadUrl("file:///android_asset/fit50/index.html")
         }
+
+        setContentView(webView)
+    }
+
+    override fun onBackPressed() {
+        if (::webView.isInitialized && webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onDestroy() {
+        if (::webView.isInitialized) {
+            webView.removeJavascriptInterface("Fit50Native")
+            webView.stopLoading()
+            webView.destroy()
+        }
+        super.onDestroy()
     }
 }
