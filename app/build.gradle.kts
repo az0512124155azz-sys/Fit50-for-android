5b8ddaebba376414938c4a91291b2cc6f3267697
Avishai Tzabary <az0512124155azz@gmail.com>
2026-09-20 13:45:13 +0300

    Add on-device ML Kit translation support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.19.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.mlkit:translate:17.0.3")
 
     implementation("androidx.credentials:credentials:1.3.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
