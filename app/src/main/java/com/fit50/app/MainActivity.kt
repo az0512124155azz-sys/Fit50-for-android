59c03e593d1dfdbe499f1b0692c063bb1fa53654
Avishai Tzabary <az0512124155azz@gmail.com>
2026-09-20 08:42:59 +0300

    Make startup logo independent of bitmap loading

--- app/src/main/java/com/fit50/app/MainActivity.kt
+++ app/src/main/java/com/fit50/app/MainActivity.kt
@@ -4,24 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             Fit50Theme {
                 var showSplash by remember { mutableStateOf(true) }
@@ -38,14 +33,7 @@ class MainActivity : ComponentActivity() {
                             .background(Forest),
                         contentAlignment = Alignment.Center
                     ) {
-                        Image(
-                            painter = painterResource(R.drawable.fit50_logo),
-                            contentDescription = "Fit50+",
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = 42.dp),
-                            contentScale = ContentScale.Fit
-                        )
+                        Fit50Logo()
                     }
                 } else {
                     Fit50App()
